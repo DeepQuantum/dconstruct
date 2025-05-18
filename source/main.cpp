4f3faf93891246f9ef9e446080e0bca51f2264d7
deepquantum <damixd02@gmail.com>
2025-05-18 12:50:13 +0200

    fixed tasks

--- source/main.cpp
+++ source/main.cpp
@@ -1,19 +1,20 @@
 #include "mainwindow.h"
 
+#include <iostream>
+
 #include <QApplication>
 #include "disassembly/disassembler.h"
+#include <cassert>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     a.setStyleSheet("Fusion");
     MainWindow w;
-
     Disassembler *dis = new Disassembler();
     dis->loadSidbase("C:/Users/damix/Documents/GitHub/TLOU2Modding/TLOU_DC_Tool_v1.01/sid1/sidbase.bin");
-    BinaryFile test("C:/Users/damix/Documents/GitHub/TLOU2Modding/tlou2_disasm/ss-ellie-hoodie-manager.bin");
+    BinaryFile test("C:/Users/damix/Documents/GitHub/TLOU2Modding/tlou2_disasm/test/ss-ellie-hoodie-manager.bin");
     dis->disassembleFile(test);
-
     w.show();
     return a.exec();
 }
