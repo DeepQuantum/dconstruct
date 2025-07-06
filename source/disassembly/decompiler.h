3f03558c4c39253cdcd4edfae5614935838b1aa3
deepquantum <damixd02@gmail.com>
2025-07-06 19:04:41 +0200

    control flow graphs

--- source/disassembly/decompiler.h
+++ source/disassembly/decompiler.h
@@ -20,13 +20,15 @@ public:
     DecompiledFunction decompile() noexcept;
     void parse_control_flow_graph() noexcept;
     void insert_new_node(u32, const ControlFlowNode &) noexcept;
-    void output_cfg_file(const std::string &path = "graph.txt") const noexcept;
+    void write_control_flow_graph_txt_file(const std::string &path = "graph.txt") const noexcept;
+    void write_control_flow_graph_image(const std::string &path = "graph.svg") const noexcept;
+    [[nodiscard]] std::string create_node_text(const ControlFlowNode *node) const noexcept;
 
 
 private:
     FunctionDisassembly *m_functionDisassembly = nullptr;
     std::map<u32, std::unique_ptr<ControlFlowNode>> m_nodes;
-    ControlFlowNode *m_controlFlowHead;
+    ControlFlowNode *m_controlFlowHead = nullptr;
 };
 
 }
