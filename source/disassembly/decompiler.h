b05687ce8822b3b7f2d7ff7edc1e9b2b4fb6d678
deepquantum <damixd02@gmail.com>
2025-07-09 00:04:07 +0200

    started loop finding algorithm

--- source/disassembly/decompiler.h
+++ source/disassembly/decompiler.h
@@ -18,17 +18,21 @@ public:
     explicit Decompiler(FunctionDisassembly *func): m_functionDisassembly(func) {};
 
     DecompiledFunction decompile() noexcept;
-    void parse_control_flow_graph() noexcept;
-    void insert_new_node(u32, const ControlFlowNode &) noexcept;
-    void write_control_flow_graph_txt_file(const std::string &path = "graph.txt") const noexcept;
-    void write_control_flow_graph_image(const std::string &path = "graph.svg") const noexcept;
-    [[nodiscard]] std::string create_node_text(const ControlFlowNode *node) const noexcept;
-
 
 private:
     FunctionDisassembly *m_functionDisassembly = nullptr;
-    std::map<u32, std::unique_ptr<ControlFlowNode>> m_nodes;
+    std::map<u32, std::unique_ptr<ControlFlowNode>> m_nodes{};
+    std::map<u32, std::vector<u32>> m_predecessors{};
+    std::map<u32, u32> m_immediateDominators{};
     ControlFlowNode *m_controlFlowHead = nullptr;
+
+    void parse_control_flow_graph() noexcept;
+    void write_control_flow_graph_txt_file(const std::string &path = "graph.txt") const noexcept;
+    void write_control_flow_graph_image(const std::string &path = "graph.svg") const noexcept;
+    [[nodiscard]] std::string create_node_text(const ControlFlowNode *) const noexcept;
+    void compute_predecessors() noexcept;
+    void find_immediate_dominators() noexcept;
+    [[nodiscard]] std::vector<u32> get_idom_predecessors(const u32 n) const noexcept;
 };
 
 }
