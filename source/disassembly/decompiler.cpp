419857ef30628075e2b59c715b9461a16ef2ced7
deepquantum <damixd02@gmail.com>
2025-07-09 00:04:07 +0200

    started loop finding algorithm

--- source/disassembly/decompiler.cpp
+++ source/disassembly/decompiler.cpp
@@ -55,53 +55,53 @@ void Decompiler::write_control_flow_graph_txt_file(const std::string &path) cons
 static std::mutex g_graphviz_mutex;
 
 void Decompiler::write_control_flow_graph_image(const std::string &path) const noexcept {
-//    GVC_t *gvc = gvContext();
-//    Agraph_t *g = agopen((char *)"G", Agdirected, nullptr);
-//    Agnode_t *current_node;
-//    std::lock_guard<std::mutex> lock(g_graphviz_mutex);
-//    u32 max_node = 0;
-//    std::map<ControlFlowNode*, Agnode_t*> node_map{};
-//    for (const auto &[start_line, node] : m_nodes) {
-//        std::string name = std::to_string(start_line);
-//        current_node = agnode(g, name.data(), 1);
-//        node_map[node.get()] = current_node;
-//        if (start_line > max_node) {
-//            max_node = start_line;
-//        }
-//        const std::string node_html_label = create_node_text(node.get());
-//
-//        agsafeset_html(current_node, const_cast<char*>("label"), node_html_label.c_str(), "");
-//
-//        agsafeset(current_node, const_cast<char*>("fontcolor"), "#8ADCFE", "");
-//        agsafeset(current_node, const_cast<char*>("shape"), "plaintext", "");
-//        agsafeset(current_node, const_cast<char*>("color"), "#8ADCFE", "");
-//    }
-//    
-//    for (const auto &[start_line, node] : m_nodes) {
-//        for (const auto &next : node->m_next) {
-//            Agedge_t *edge = agedge(g, node_map[node.get()], node_map[next], const_cast<char*>(""), 1);
-//            if (node->m_endLine + 1 == next->m_startLine) {
-//                agsafeset(edge, const_cast<char*>("color"), "purple", "");
-//            } else if (next->m_startLine < node->m_startLine) {
-//                agsafeset(edge, const_cast<char*>("color"), "red", "");
-//            } else if (node->m_lines.back().m_instruction.opcode == Opcode::Branch) {
-//                agsafeset(edge, const_cast<char*>("color"), "blue", "");
-//            } else {
-//                agsafeset(edge, const_cast<char*>("color"), "#8ADCFE", "");
-//            }
-//        }
-//    }
-//    Agraph_t *returng = agsubg(g, const_cast<char*>("return"), 1);
-//    Agnode_t *return_node = agnode(returng, const_cast<char*>(std::to_string(max_node).c_str()), 1);
-//    agsafeset(return_node, const_cast<char*>("peripheries"), "1", "");
-//    agsafeset(returng, const_cast<char*>("rank"), "max", "");
-//    agsafeset(g, const_cast<char*>("bgcolor"), "#0F0F0F", const_cast<char*>(""));
-//    agsafeset(g, const_cast<char*>("splines"), "ortho", const_cast<char*>(""));
-//    gvLayout(gvc, g, "dot");
-//    gvRenderFilename(gvc, g, "svg", path.c_str());
-//    gvFreeLayout(gvc, g);
-//    agclose(g);
-//    gvFreeContext(gvc);
+   GVC_t *gvc = gvContext();
+   Agraph_t *g = agopen((char *)"G", Agdirected, nullptr);
+   Agnode_t *current_node;
+   std::lock_guard<std::mutex> lock(g_graphviz_mutex);
+   u32 max_node = 0;
+   std::map<ControlFlowNode*, Agnode_t*> node_map{};
+   for (const auto &[start_line, node] : m_nodes) {
+       std::string name = std::to_string(start_line);
+       current_node = agnode(g, name.data(), 1);
+       node_map[node.get()] = current_node;
+       if (start_line > max_node) {
+           max_node = start_line;
+       }
+       const std::string node_html_label = create_node_text(node.get());
+
+       agsafeset_html(current_node, const_cast<char*>("label"), node_html_label.c_str(), "");
+
+       agsafeset(current_node, const_cast<char*>("fontcolor"), "#8ADCFE", "");
+       agsafeset(current_node, const_cast<char*>("shape"), "plaintext", "");
+       agsafeset(current_node, const_cast<char*>("color"), "#8ADCFE", "");
+   }
+   
+   for (const auto &[start_line, node] : m_nodes) {
+       for (const auto &next : node->m_next) {
+           Agedge_t *edge = agedge(g, node_map[node.get()], node_map[next], const_cast<char*>(""), 1);
+           if (node->m_endLine + 1 == next->m_startLine) {
+               agsafeset(edge, const_cast<char*>("color"), "purple", "");
+           } else if (next->m_startLine < node->m_startLine) {
+               agsafeset(edge, const_cast<char*>("color"), "red", "");
+           } else if (node->m_lines.back().m_instruction.opcode == Opcode::Branch) {
+               agsafeset(edge, const_cast<char*>("color"), "blue", "");
+           } else {
+               agsafeset(edge, const_cast<char*>("color"), "#8ADCFE", "");
+           }
+       }
+   }
+   Agraph_t *returng = agsubg(g, const_cast<char*>("return"), 1);
+   Agnode_t *return_node = agnode(returng, const_cast<char*>(std::to_string(max_node).c_str()), 1);
+   agsafeset(return_node, const_cast<char*>("peripheries"), "1", "");
+   agsafeset(returng, const_cast<char*>("rank"), "max", "");
+   agsafeset(g, const_cast<char*>("bgcolor"), "#0F0F0F", const_cast<char*>(""));
+   agsafeset(g, const_cast<char*>("splines"), "ortho", const_cast<char*>(""));
+   gvLayout(gvc, g, "dot");
+   gvRenderFilename(gvc, g, "svg", path.c_str());
+   gvFreeLayout(gvc, g);
+   agclose(g);
+   gvFreeContext(gvc);
 }
 
 void Decompiler::parse_control_flow_graph() noexcept {
@@ -156,4 +156,48 @@ void Decompiler::parse_control_flow_graph() noexcept {
     }
     m_controlFlowHead = m_nodes[0].get();
 }
+
+void Decompiler::compute_predecessors() noexcept {
+    for (const auto &[start_line, node] : m_nodes) {
+        for (const auto &successor : node->m_next) {
+            m_predecessors[successor->m_startLine].push_back(start_line);
+        }
+    }
+}
+
+[[nodiscard]] std::vector<u32> Decompiler::get_idom_predecessors(const u32 n) const noexcept {
+    std::vector<u32> res{};
+    for (const auto &p : m_predecessors.at(n)) {
+        if (m_immediateDominators.contains(p)) {
+            res.push_back(p);
+        }
+    }
+    return res;
+}
+
+void Decompiler::find_immediate_dominators() noexcept {
+    compute_predecessors();
+    m_immediateDominators[0] = 0;
+
+    b8 changed = true;
+    while (changed) {
+        changed = false;
+        for (u32 n = 1; n < m_nodes.size(); ++n) {
+            const std::vector<u32> &idom_preds = get_idom_predecessors(n);
+            if (idom_preds.empty()) {
+                continue;
+            }
+            u32 new_idom = idom_preds[0];
+            for (u32 p = 1; p < idom_preds.size(); ++p) {
+                new_idom = intersect(new_idom, p);
+            }
+            if (m_immediateDominators.at(n) != new_idom) {
+                m_immediateDominators[n] = new_idom;
+                changed = true;
+            }
+        }
+    }
+}
+
+
 }
