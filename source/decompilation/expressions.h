7066ff7864ac1b0afc19d41d1d7c12e5fa970194
deepquantum <damixd02@gmail.com>
2025-07-22 18:10:45 +0200

    tons of work on decompiler & testing

--- source/decompilation/expressions.h
+++ source/decompilation/expressions.h
@@ -6,29 +6,28 @@
 
 namespace dconstruct::dcompiler {
 
-    
-
     struct expression {
     public:
         virtual void pseudo(std::ostream&) const noexcept = 0;
         virtual void ast(std::ostream&) const noexcept = 0;
-        virtual std::unique_ptr<const expression> eval() const noexcept = 0;
+        [[nodiscard]] virtual std::unique_ptr<const expression> eval() const noexcept = 0;
+        [[nodiscard]] virtual b8 operator==(const expression &rhs) const noexcept = 0;
     };
 
-    
+    using expression_uptr = std::unique_ptr<const expression>;
 
     struct unary_expr : public expression {
     public:
-        unary_expr(std::unique_ptr<const expression> lhs) : m_lhs(std::move(lhs)) {};
+        unary_expr(expression_uptr lhs) : m_lhs(std::move(lhs)) {};
 
     protected:
-        std::unique_ptr<const expression>m_lhs;
+        expression_uptr m_lhs;
     };
 
     struct call_expr : public expression {
     private:
-        const expression* m_callee;
-        std::vector<const expression*> m_arguments;
+        expression_uptr m_callee;
+        std::vector<expression_uptr> m_arguments;
 
         void pseudo(std::ostream &os) const noexcept final;
         void ast(std::ostream &os) const noexcept final;
@@ -36,7 +35,8 @@ namespace dconstruct::dcompiler {
 
     template<typename T>
     struct literal : public expression {
-        const T& value() const noexcept;
+        const T& get_value() const noexcept { return m_value; }
+        [[nodiscard]] b8 operator==(const expression &rhs) const noexcept override;
     protected:
         T m_value;
     };
@@ -48,12 +48,25 @@ namespace dconstruct::dcompiler {
 
         void pseudo(std::ostream& os) const noexcept final;
         void ast(std::ostream& os) const noexcept final;
-        std::unique_ptr<const expression> eval() const noexcept final;
+        [[nodiscard]] expression_uptr eval() const noexcept final;
     };
     
     struct string_literal : public literal<std::string> {
         void pseudo(std::ostream& os) const noexcept final;
         void ast(std::ostream& os) const noexcept final;
-        std::unique_ptr<const expression> eval() const noexcept final;
+        [[nodiscard]] expression_uptr eval() const noexcept final;
+    };
+
+    struct identifier : public expression {
+        identifier(const std::string &name, const u8 idx) : m_name(name), m_idx(idx) {};
+        void pseudo(std::ostream& os) const noexcept final;
+        void ast(std::ostream& os) const noexcept final;
+        [[nodiscard]] expression_uptr eval() const noexcept final;
+        [[nodiscard]] b8 operator==(const expression &rhs) const noexcept override;
+
+
+    private:
+        std::string m_name;
+        u8 m_idx;        
     };
 };
--- test/decompiler_test.cpp
+++ test/decompiler_test.cpp
@@ -1,25 +1,51 @@
 #include "decompiler.h"
 #include "binaryfile.h"
 #include "file_disassembler.h"
+#include "binary_expressions.h"
+#include <array>
 #include <gtest/gtest.h>
 #include <filesystem>
 
-#define GET_FUNC(p, o) 
+const std::string TEST_DIR = "C:/Users/damix/Documents/GitHub/TLOU2Modding/dconstruct/test/dc_test_files/";
 
 namespace dconstruct::testing {
 
     static function_disassembly get_function_disassembly(const std::string &path, const u32 offset) {
         SIDBase base{}; 
-        base.load("dc_test_files/test_sidbase.bin"); 
-        BinaryFile file(path); 
+        base.load(TEST_DIR + "test_sidbase.bin"); 
+        BinaryFile file(TEST_DIR + path); 
+        file.dc_setup();
         FileDisassembler disassembler(&file, &base, "", DisassemblerOptions{}); 
         const ScriptLambda *lambda_ptr = disassembler.get_value_ptr_at<ScriptLambda>(offset); 
         const function_disassembly fd = disassembler.create_function_disassembly(lambda_ptr);
+        return fd;
+    }
+
+    template<std::size_t array_size>
+    dcompiler::expression_frame make_expression_frame(std::array<Instruction, array_size> istrs) {
+        const StackFrame sf{};
+        std::vector<function_disassembly_line> lines{};
+        for (u32 i = 0; i < array_size; ++i) {
+            lines.emplace_back(i, &istrs[i]);
+        }
+        function_disassembly fd{std::move(lines), std::move(sf), "Test"};
+        dcompiler::Decompiler dc(&fd);
+        std::vector<dcompiler::decompiled_function> funcs = dc.decompile();
+        return std::move(funcs[0].m_frame);
     }
 
     TEST(DECOMPILER, BasicAdd) {
-        const function_disassembly fd = get_function_disassembly("dc_test_files/ss-fx-funcs.bin", 0x60);
-        ASSERT_EQ(fd.m_lines[0].m_instruction.opcode, Opcode::Move);
-    }
+        const std::array<Instruction, 1> istrs = {
+            {Opcode::LoadU16Imm, 0, 1, 0},
+        };
+        const dcompiler::expression_frame& frame = make_expression_frame(istrs);
 
+        const auto& actual = *dynamic_cast<const dcompiler::assign_expr*>(frame.m_expressions.at(0).get());
+        const auto& expected = *std::make_unique<const dcompiler::assign_expr>
+        (
+            std::move(std::make_unique<const dcompiler::identifier>("Test", 0)),
+            std::move(std::make_unique<const dcompiler::num_literal>(1))
+        );
+        ASSERT_EQ(actual, expected);
+    }
 }
