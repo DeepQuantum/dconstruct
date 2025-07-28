f8b4197851fe3672c0de8337e01ee32c6de1f87a
deepquantum <damixd02@gmail.com>
2025-07-28 23:28:25 +0200

    type fixes

--- source/ast/type.h
+++ source/ast/type.h
@@ -3,10 +3,40 @@
 #include "base.h"
 #include "tokens.h"
 
-namespace dconstruct::compiler {
-struct type {
-    type(const compiler::token &type) : m_type(type) {};
-private:
-    compiler::token m_type;
-};
+
+namespace dconstruct::ast {
+
+    enum primitive : u8 {
+        E_UNKNOWN,
+        E_INT,
+        E_FLOAT,
+        E_STRING,
+        E_PTR,
+        E_HASH,
+    };
+
+
+    struct type : std::variant<std::string, primitive> {
+        type(const compiler::token& type) : m_value(type.m_lexeme) {};
+        type(const primitive &type) : m_value(type) {};
+
+        std::variant<std::string, primitive> m_value;
+
+        std::string to_string() const noexcept {
+            if (m_value.index() == 0) {
+                return std::get<std::string>(m_value);
+            }
+            else {
+                switch (std::get<primitive>(m_value)) {
+                    case E_UNKNOWN: return "var";
+                    case E_INT: return "i32";
+                    case E_FLOAT: return "f32";
+                    case E_STRING: return "string";
+                    case E_PTR: return "p64";
+                    case E_HASH: return "sid64";
+                    default: return "var";
+                }
+            }
+        }
+    };
 }
