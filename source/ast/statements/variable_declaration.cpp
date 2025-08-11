1daf5a2e0da2329fa9cdb5dd90cb08b5d11e5d4b
deepquantum <damixd02@gmail.com>
2025-08-11 06:44:01 +0200

    type stuff

---
+++ source/ast/statements/variable_declaration.cpp
@@ -0,0 +1,31 @@
+#include "ast/statements/variable_declaration.h"
+
+namespace dconstruct::ast {
+
+void variable_declaration::pseudo(std::ostream& os) const {
+    os << kind_to_string(m_type) << m_identifier;
+    if (m_init != nullptr) {
+        os << m_init;
+    }
+    os << ';';
+}
+
+void variable_declaration::ast(std::ostream& os) const {
+    os << "variable_declaration[" << kind_to_string(m_type) << ", " << m_identifier << ", ";
+    if (m_init != nullptr) {
+        os << '{' << m_init << '}';
+    } else {
+        os << "null";
+    }
+    os << ';';
+}
+
+[[nodiscard]] b8 variable_declaration::equals(const statement& rhs) const noexcept {
+    const variable_declaration* rhs_ptr = dynamic_cast<const variable_declaration*>(&rhs); 
+    if (rhs_ptr == nullptr) { 
+        return false; 
+    }
+    return m_type == rhs_ptr->m_type && m_identifier == rhs_ptr->m_identifier && *m_init == *rhs_ptr->m_init;
+}
+
+}
