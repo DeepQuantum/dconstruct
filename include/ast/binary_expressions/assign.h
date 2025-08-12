e23fdaa15f11a050574b604029d26bd5e79f7a5a
qntm <damixd20@gmail.com>
2025-08-12 06:48:30 +0200

    fixups

--- include/ast/binary_expressions/assign.h
+++ include/ast/binary_expressions/assign.h
@@ -7,6 +7,7 @@ namespace dconstruct::ast {
     struct assign_expr : public clonable_binary_expr<assign_expr> {
         using clonable_binary_expr::clonable_binary_expr;
         [[nodiscard]] std::unique_ptr<expression> simplify() const final;
+        [[nodiscard]] std::optional<ast::full_type> compute_type(const compiler::environment& env) const final;
 
     private:
         [[nodiscard]] std::string get_op_char() const noexcept final { return "="; }
