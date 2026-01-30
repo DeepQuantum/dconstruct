07f011e05db68c3015c44fa0c6f4fca359aa9637
deepquantum <damixd02@gmail.com>
2026-01-30 18:41:18 +0100

    path  fix

--- include/compilation/preprocessor.h
+++ include/compilation/preprocessor.h
@@ -7,17 +7,18 @@
 #include "cxxopts.hpp"
 
 namespace dconstruct::compilation {
-    struct required_options {
+    struct compiler_options {
         std::filesystem::path m_target;
         std::filesystem::path m_output;
         std::filesystem::path m_modules;
         std::filesystem::path m_sidbase;
+        std::optional<std::filesystem::path> m_repackage;
 
         
-        [[nodiscard]] static std::expected<required_options, std::string> parse(const cxxopts::ParseResult& args, std::string& source) noexcept;
+        [[nodiscard]] static std::expected<compiler_options, std::string> parse(const cxxopts::ParseResult& args, std::string& source) noexcept;
     private:
-        [[nodiscard]] static std::expected<required_options, std::string> from_args(const cxxopts::ParseResult& args) noexcept;
-        [[nodiscard]] static std::expected<required_options, std::string> from_dcpl(std::string& source) noexcept;
+        [[nodiscard]] static std::expected<compiler_options, std::string> from_args(const cxxopts::ParseResult& args) noexcept;
+        [[nodiscard]] static std::expected<compiler_options, std::string> from_dcpl(std::string& source) noexcept;
     };
 
 }
