5c29aeb80effca937638c3930138f3f0d187128c
deepquantum <damixd02@gmail.com>
2026-05-10 04:10:13 +0200

    is-final-build hook

--- source/hooking/hooking.cpp
+++ source/hooking/hooking.cpp
@@ -1,67 +1,88 @@
 #include "base.h"
 #include <windows.h>
 #include "MinHook.h"
+#include <filesystem>
 #include <format>
 #include <print>
 #include <fstream>
+#include <mutex>
 #include <string>
-#include <iostream>
+#include <system_error>
+#include <utility>
 #include "disassembly/instructions.h"
 
 
 namespace dconstruct::hooking {
     using QWORD = DWORD64;
 
-    using target_fn_t = QWORD*(__fastcall*)(QWORD* a1, QWORD* a2, i64* a3);
+    template <typename... Args>
+    void log(std::format_string<Args...> fmt, Args&&... args) {
+        static std::mutex log_mutex;
+        std::scoped_lock lock(log_mutex);
 
-    target_fn_t original_fn = nullptr;
+        std::ofstream out("C:/Program Files (x86)/Steam/steamapps/common/The Last of Us Part II/dconstruct_log.txt", std::ios::out | std::ios::app);
+        if (!out) {
+            return;
+        }
 
-    QWORD* __fastcall hook(QWORD* a1, QWORD* a2, i64* a3) {
-        std::ofstream out("hook.log", std::ios::out | std::ios::app);
-
-        const p64 instruction_idx_ptr = *(a1 + 1);
-        const Instruction* instruction_ptr = *(Instruction**)instruction_idx_ptr;
-        u64 i = 0;
-        do {
-            const Instruction& istr = instruction_ptr[i++];
-            std::println(out, "{}, {}, {:x}, {:x}, {:x}" , i, istr.opcode_to_string(), istr.destination, istr.operand1, istr.operand2);
-        } while(instruction_ptr[i].opcode != Opcode::Return);
-        
-        
-        const Instruction first_instruction = instruction_ptr[0];
-
-
-        // u64** v9 = (u64 **)(test + 8);
-
-        // std::println(out, "v9: {:x}" , p64(v9));
-
-        //const u64 op2 = (u64)*(u32*)*start_ptr;
-
-        //out <<  "here2" << "\n";
-
-        //const Opcode opcode = *(Opcode *)(local_2c8 + op2 * 8);
-
-        //const std::string log_msg = std::format("{}\n",(u8)opcode);
-        //out << log_msg << "\n";
-        return original_fn(a1, a2, a3);
+        std::println(out, fmt, std::forward<Args>(args)...);
     }
 
-    static constexpr p64 IMAGE_BASE               = 0x140000000;
-    static constexpr p64 FUNCTION_RELATIVE_OFFSET = 0x1414B6940;
+    QWORD* __fastcall is_final_build_hook(QWORD*);
 
-    static constexpr p64 FUNCTION_OFFSET          = FUNCTION_RELATIVE_OFFSET - IMAGE_BASE; 
+    decltype(&is_final_build_hook) is_final_build_orig = nullptr;
+
+    // QWORD* __fastcall hook(QWORD* a1, QWORD* a2, i64* a3) {
+    //     std::ofstream out("hook.log", std::ios::out | std::ios::app);
+
+    //     const p64 instruction_idx_ptr = *(a1 + 1);
+    //     const Instruction* instruction_ptr = *(Instruction**)instruction_idx_ptr;
+    //     u64 i = 0;
+    //     do {
+    //         const Instruction& istr = instruction_ptr[i++];
+    //         std::println(out, "{}, {}, {:x}, {:x}, {:x}" , i, istr.opcode_to_string(), istr.destination, istr.operand1, istr.operand2);
+    //     } while(instruction_ptr[i].opcode != Opcode::Return);
+        
+        
+    //     const Instruction first_instruction = instruction_ptr[0];
+
+
+    //     // u64** v9 = (u64 **)(test + 8);
+
+    //     // std::println(out, "v9: {:x}" , p64(v9));
+
+    //     //const u64 op2 = (u64)*(u32*)*start_ptr;
+
+    //     //out <<  "here2" << "\n";
+
+    //     //const Opcode opcode = *(Opcode *)(local_2c8 + op2 * 8);
+
+    //     //const std::string log_msg = std::format("{}\n",(u8)opcode);
+    //     //out << log_msg << "\n";
+    //     return original_fn(a1, a2, a3);
+    // }
+
+    QWORD* __fastcall is_final_build_hook(QWORD* a1) {
+       // log("called is_final_build?");
+       // char orig_res = is_final_build_orig(a1, a2, a3, a4, a5);
+        *a1 = false;
+        return a1;
+    }
+
+    static constexpr p64 IMAGE_BASE                 = 0x140000000;
+    static constexpr p64 RUN_SCRIPT_RELATIVE_OFFSET = 0x140D4B800;
+
+    static constexpr p64 RUN_SCRIPT_OFFSET          = RUN_SCRIPT_RELATIVE_OFFSET - IMAGE_BASE; 
 
     DWORD WINAPI init_thread(void*) {
         MH_Initialize();
         const p64 base = (p64)GetModuleHandle(nullptr);
-        void* target = (void*)(base + FUNCTION_OFFSET);
-
-        MH_CreateHook(target, &hook, reinterpret_cast<void**>(&original_fn));
-        
-        MH_EnableHook(target);
+        void* is_final_build_target = (void*)(base + RUN_SCRIPT_OFFSET);
+        MH_CreateHook(is_final_build_target, &is_final_build_hook, reinterpret_cast<void**>(&is_final_build_orig));
+        MH_EnableHook(is_final_build_target);
         return 0;
     }
-}   
+}
 
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD reason, LPVOID) {
     if (reason == DLL_PROCESS_ATTACH) {
